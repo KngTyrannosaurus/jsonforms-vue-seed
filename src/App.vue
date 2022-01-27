3b1202a09543743ad4b1fec901475ae1c6a18b88
KngTyrannosaurus <daragh.dod@gmail.com>
2022-01-27 14:12:12 +1100

    working now

--- src/App.vue
+++ src/App.vue
@@ -1,209 +1,168 @@
 <template>
   <h1>Darkhorse Studios JSON Editor</h1>
   <div class="myform">
- <input type="file" ref="file" @change="fileToSchema(evt)" />
- <button @click="testUpdateSchema()">Test update the schema</button>
+    <input
+      type="file"
+      ref="file"
+      @change="
+        {
+          fileToSchema($event);
+        }
+      "
+    />
+    <hr class="section-divider" />
     <json-forms
       :data="data"
       :renderers="renderers"
-      :schema="mySchema"
-      :uischema="uischema"
+      :schema="schema"
       @change="onChange"
     />
-    <button @click="downloadData()"> Download Filled Schema</button>
+    <hr class="section-divider" />
+    <button @click="downloadData()">Download Filled Schema</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { JsonForms, JsonFormsChangeEvent } from "@jsonforms/vue";
-import {
-  defaultStyles,
-  mergeStyles,
-  vanillaRenderers,
-} from "@jsonforms/vue-vanilla";
+  import { defineComponent } from "vue";
+  import { JsonForms, JsonFormsChangeEvent } from "@jsonforms/vue";
+  import {
+    defaultStyles,
+    mergeStyles,
+    vanillaRenderers,
+  } from "@jsonforms/vue-vanilla";
 
-// mergeStyles combines all classes from both styles definitions into one
-const myStyles = mergeStyles(defaultStyles, { control: { label: "mylabel" } });
+  // mergeStyles combines all classes from both styles definitions into one
+  const myStyles = mergeStyles(defaultStyles, {
+    control: { label: "mylabel" },
+  });
 
-const renderers = [
-  ...vanillaRenderers,
-  // here you can add custom renderers
-];
+  const renderers = [
+    ...vanillaRenderers,
+    // here you can add custom renderers
+  ];
 
-const schema = {
-  properties: {
-    name: {
-      type: "string",
-      minLength: 1,
-      description: "The task's name"
+  const schema = {
+    properties: {
+      name: {
+        type: "string",
+        minLength: 1,
+        description: "The task's name",
+      },
+      description: {
+        title: "Long Description",
+        type: "string",
+        many: true,
+      },
+      done: {
+        type: "boolean",
+      },
     },
-    description: {
-      title: "Long Description",
-      type: "string",
-    },
-    done: {
-      type: "boolean",
-    },
-    dueDate: {
-      type: "string",
-      format: "date",
-      description: "The task's due date"
-    },
-    rating: {
-      type: "integer",
-      maximum: 5,
-    },
-    recurrence: {
-      type: "string",
-      enum: ["Never", "Daily", "Weekly", "Monthly"]
-    },
-    recurrenceInterval: {
-      type: "integer",
-      description: "Days until recurrence"
-    },
-  },
-};
+  };
 
-const uischema = {
-  type: "HorizontalLayout",
-  elements: [
-    {
-      type: "VerticalLayout",
-      elements: [
-        {
-          type: "Control",
-          scope: "#/properties/name",
-        },
-        {
-          type: "Control",
-          scope: "#/properties/description",
-          options: {
-            multi: true,
-          }
-        },
-        {
-          type: "Control",
-          scope: "#/properties/done",
-        },
-      ],
+  export default defineComponent({
+    name: "App",
+    components: {
+      JsonForms,
     },
-    {
-      type: "VerticalLayout",
-      elements: [
-        {
-          type: "Control",
-          scope: "#/properties/dueDate",
+    data() {
+      return {
+        // freeze renderers for performance gains
+        renderers: Object.freeze(renderers),
+        data: {
+          // name: "Send email to Adrian",
+          // description: "Confirm if you have passed the subject\nHereby ...",
+          // done: true,
+          // recurrence: "Daily",
+          // rating: 3,
         },
-        {
-          type: "Control",
-          scope: "#/properties/rating",
-        },
-        {
-          type: "Control",
-          scope: "#/properties/recurrence",
-        },
-        {
-          type: "Control",
-          scope: "#/properties/recurrenceInterval",
-        },
-      ],
+        schema,
+      };
     },
-  ],
-};
 
-export default defineComponent({
-  name: "App",
-  components: {
-    JsonForms,
-  },
-  data() {
-    return {
-      // freeze renderers for performance gains
-      renderers: Object.freeze(renderers),
-      data: {
-        name: "Send email to Adrian",
-        description: "Confirm if you have passed the subject\nHereby ...",
-        done: true,
-        recurrence: "Daily",
-        rating: 3,
+    methods: {
+      fileToSchema(event: any) {
+        // Single-file input, so we're safe to grab [0] (FileList has no 'pop' command)
+        const file = event.target.files[0];
+        const fileReader = new FileReader();
+
+        // Update schema on parse
+        fileReader.addEventListener(
+          "load",
+          () => {
+            this.data = {}; // Wipe the data prop, otherwise ghosts from the last form will be in there.
+            const result = String(fileReader.result); // Coerce result to string (fileReaders make lots of non-stringy outputs on fail)
+            this.schema = JSON.parse(result);
+          },
+          false
+        );
+
+        fileReader.readAsText(file);
       },
 
-      schema,
-      uischema,
-      mySchema: {}
-    };
-  },
-  methods: {
-   
-  fileToSchema(file: File){
-    const fileReader = new FileReader()
-    fileReader.onload = function ( evt ) { console.log(evt) };
-    fileReader.readAsText( file );
-  },
-    testUpdateSchema(){
-  this.mySchema = {properties:{name:{type: "string", minLength: 1, description: "Blah blah blah"}}}
+      downloadData() {
+        const data = JSON.stringify(this.data, null, 2);
+        const filename = "JSONData.json";
+        const blob = new Blob([data], { type: "text/plain;charset=utf-8;" });
+        if (navigator.msSaveBlob) {
+          // IE 10+
+          navigator.msSaveBlob(blob, filename);
+        } else {
+          const link = document.createElement("a");
+          if (link.download !== undefined) {
+            // feature detection
+            // Browsers that support HTML5 download attribute
+            const url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = "hidden";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+          }
+        }
+      },
+      onChange(event: JsonFormsChangeEvent) {
+        this.data = event.data;
+      },
     },
-    downloadData(){
-      const data = JSON.stringify(this.data)
-      const filename = 'JSONData.json'
-     const blob = new Blob([data], { type: 'text/plain;charset=utf-8;' })
-  if (navigator.msSaveBlob) { // IE 10+
-    navigator.msSaveBlob(blob, filename)
-  } else {
-    const link = document.createElement('a')
-    if (link.download !== undefined) { // feature detection
-      // Browsers that support HTML5 download attribute
-      const url = URL.createObjectURL(blob)
-      link.setAttribute('href', url)
-      link.setAttribute('download', filename)
-      link.style.visibility = 'hidden'
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-    }
-  }
+    provide() {
+      return {
+        styles: myStyles,
+      };
     },
-    onChange(event: JsonFormsChangeEvent) {
-      this.data = event.data;
-      
-    },
-  },
-  provide() {
-    return {
-      styles: myStyles,
-    };
-  },
-});
+  });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  margin-left: 120px;
-  margin-right: 120px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    margin-left: 120px;
+    margin-right: 120px;
+  }
+  .section-divider {
+    margin: 50px 0 50px 0;
+    width: 100%;
+  }
+  .mylabel {
+    color: darkslategrey;
+  }
 
-.mylabel {
-  color: darkslategrey;
-}
+  .vertical-layout {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
 
-.vertical-layout {
-  margin-left: 10px;
-  margin-right: 10px;
-}
+  .myform {
+    width: 640px;
+    margin: 0 auto;
+  }
 
-.myform {
-  width: 640px;
-  margin: 0 auto;
-}
-
-.text-area {
-  min-height: 80px;
-}
+  .text-area {
+    min-height: 80px;
+  }
 </style>
